675c65f38a598d2a3238c7dd16d7887222ef47d3
slashformotion <slashformotion@protonmail.com>
2022-10-21 16:30:12 +0200

    Updating build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
-echo "" > uBlacklist.txt
+#!/bin/bash
+
+target="uBlacklist.txt"
+rm -f $target
+touch $target
 while read p; do
   echo "Processing file at $p"
-  curl $p >> uBlacklist.txt
+  curl $p >> ${target}
 done < sources.txt
+
+echo "Appending my personal list"
+cat personalList.txt >> ${target}
 
 echo Done
