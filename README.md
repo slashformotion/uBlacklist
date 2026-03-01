434bb5adf2367689db450456027cdb0a16000b74
slashformotion <slashformotion@protonmail.com>
2026-03-01 15:40:37 +0100

    add a list and cleaning

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ https://raw.githubusercontent.com/slashformotion/uBlacklist/main/uBlacklist.txt
 - [github-translation](https://github.com/arosh/ublacklist-github-translation)
 - [stackoverflow-translation](https://github.com/arosh/ublacklist-stackoverflow-translation)
 - [content-farm-list](https://github.com/wdmpa/content-farm-list)
+- [content-farm-ai](https://github.com/agsimmons/ai-content-blocklist/)
 - Personal additions in `personalList.txt`
 
 ## Build
--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@ target="uBlacklist.txt"
 rm -f $target
 touch $target
 echo "Starting to pull from sources.txt"
-while read p; do
+while read -r p; do
   echo "Processing file at $p"
-  curl $p >> ${target}
+  curl "$p" >> ${target}
 done < sources.txt
 
 echo "Appending my personal list"
 cat personalList.txt >> ${target}
+
+echo "Removing duplicates and comments"
+sort -u ${target} | grep -v '^#' | grep -v '^$' > ${target}.tmp
+mv ${target}.tmp ${target}
 
 echo "Done"
