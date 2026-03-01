08c954b340534edc59913a84f0300e1f1352fcd6
slashformotion <slashformotion@protonmail.com>
2026-03-01 15:35:14 +0100

    chore: housekeeping

---
+++ README.md
@@ -0,0 +1,25 @@
+# uBlacklist
+
+Aggregated blocklist for [uBlacklist](https://github.com/iorate/uBlacklist) browser extension.
+
+## Usage
+
+Subscribe to the raw list URL:
+```
+https://raw.githubusercontent.com/slashformotion/uBlacklist/main/uBlacklist.txt
+```
+
+## Sources
+
+- [github-translation](https://github.com/arosh/ublacklist-github-translation)
+- [stackoverflow-translation](https://github.com/arosh/ublacklist-stackoverflow-translation)
+- [content-farm-list](https://github.com/wdmpa/content-farm-list)
+- Personal additions in `personalList.txt`
+
+## Build
+
+```bash
+./build.sh
+```
+
+Auto-updates daily via CI.
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        # nativeBuildInputs is usually what you want -- tools you need to run
+        nativeBuildInputs = with pkgs; [
+          gnumake
+          curl
+          bash
+        ];
+      };
+    });
+  };
+}
